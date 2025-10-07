d85454c8997ecf0f829dae25375a14e9db1a4ecf
DaShy72 <141749109+DaShy72@users.noreply.github.com>
2025-10-07 20:25:14 +0300

    create routes

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
