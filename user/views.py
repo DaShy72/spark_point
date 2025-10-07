b119b2ceb84100c379c56ddb7e79c5943a8ddc0a
DaShy72 <141749109+DaShy72@users.noreply.github.com>
2025-10-07 14:55:33 +0300

    Create views for login, logout and registration

--- user/views.py
+++ user/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
+
+
+def register_view(request):
+    if request.methood == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm
+    return render(request, 'user/register.html', {'form': form})
+
+
+
+def login_view(request):
+    if request.methood == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            next_url = request.GET.get('next') or '/'
+            return redirect(next_url)
+    else:
+        form = CustomAuthenticationForm()
+
+    return render(request, 'user/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 def index(request):
     return render(request, 'main_app/index.html')
