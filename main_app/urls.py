c80b43e1599c38c0d1f2a039c2e11cfd154b343d
DaShy72 <141749109+DaShy72@users.noreply.github.com>
2025-10-09 17:10:43 +0300

    create new urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('page/<str:name>', views.my_page, name='my_page'),
+    path('create', views.create_page, name='create'),
+
 ]
