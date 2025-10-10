0b30b81273e4b9c4e7cd7ba8b8f97864b7960765
DaShy72 <141749109+DaShy72@users.noreply.github.com>
2025-10-10 16:13:13 +0300

    Change models for Info for page

--- main_app/models.py
+++ main_app/models.py
@@ -3,17 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Projects(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
     image_url = models.ImageField(upload_to='uploads/')
-    url_project = models.TextField()
-
-class Contacts(models.Model):
-    mail = models.CharField(max_length=100)
-    tiktok = models.CharField(max_length=100, null=True)
-    telegram = models.CharField(max_length=100, null=True)
-    instagram = models.CharField(max_length=100, null=True)
-    facebook = models.CharField(max_length=100, null=True)
-    tel = models.CharField(max_length=100, null=True)
 
 class InfoForPage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,8 +14,12 @@ class InfoForPage(models.Model):
     last_name = models.CharField(max_length=200)
     type_of_activity = models.TextField(max_length=100)
     about = models.TextField(max_length=500)
-    projects = models.ForeignKey(Projects, on_delete=models.CASCADE)
-    contacts = models.ForeignKey(Contacts, on_delete=models.CASCADE)
     template_name = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    tiktok = models.CharField(max_length=100, null=True)
+    telegram = models.CharField(max_length=100, null=True)
+    instagram = models.CharField(max_length=100, null=True)
+    facebook = models.CharField(max_length=100, null=True)
+    tel = models.CharField(max_length=100, null=True)
 
 
