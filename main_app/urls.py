8da26d6a369e410de1f71745c238acf76d138097
DaShy72 <141749109+DaShy72@users.noreply.github.com>
2025-10-15 14:56:48 +0300

    Create routs for examles

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('page/<str:name>', views.user_page, name='user_page'),
     path('create', views.create_page, name='create'),
     path('cabinet', views.cabinet, name='cabinet'),
+
+    #examples
+    path('bill_gates', views.bill_gates, name='bill_gates'),
+    path('curtis_james_jackson', views.curtis_james_jackson, name='curtis_james_jackson'),
+    path('leonardo_dicaprio', views.leonardo_dicaprio, name='leonardo_dicaprio'),
+    path('homer_jay_simpson', views.homer_jay_simpson, name='homer_jay_simpson'),
+    path('matthew_david_mcconaughey', views.matthew_david_mcconaughey, name='matthew_david_mcconaughey'),
 ]
